e5cc916f71acc10d8f92820f68beff6b9dd977be
PramodJana <pramodjana395@gmail.com>
2019-01-11 23:37:32 +0530

    Introduction to express framework

--- index.js
+++ index.js
@@ -1,37 +1,77 @@
-const http = require('http')
+const express = require('express')
 
-const fs = require('fs')
+const app = express()
 
+app.get('/',(request,response)=>{
 
-const aboutpage = fs.readFileSync('about.html')
+response.json({
 
-const contactpage = fs.readFileSync('contact.html')
-
-const homepage = fs.readFileSync('index.html')
-
-const server = http.createServer((request,response)=>{
-console.log(request.url)
-
-if(request.url === '/about'){
-  return response.end(aboutpage)
-}
-else if (request.url === '/contact'){
-  return response.end(contactpage)
-}
-else if(request.url==='/')
-{
-  return response.end(homepage)
-}
-else {
-
-  response.writeHead(404)
-  response.end('The Page was not found')
-
-}
-
-
-
+  name:'Pramod Jana'
+})
 
 })
 
-server.listen(3000)
+app.get('/about',(request,response)=>{
+
+response.send({
+
+  name:'Pramod Jana'
+})
+
+})
+
+
+app.listen(3000,()=>{
+
+console.log('App listening on port 3000')
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+// const http = require('http')
+//
+// const fs = require('fs')
+//
+//
+// const aboutpage = fs.readFileSync('about.html')
+//
+// const contactpage = fs.readFileSync('contact.html')
+//
+// const homepage = fs.readFileSync('index.html')
+//
+// const server = http.createServer((request,response)=>{
+// console.log(request.url)
+//
+// if(request.url === '/about'){
+//   return response.end(aboutpage)
+// }
+// else if (request.url === '/contact'){
+//   return response.end(contactpage)
+// }
+// else if(request.url==='/')
+// {
+//   return response.end(homepage)
+// }
+// else {
+//
+//   response.writeHead(404)
+//   response.end('The Page was not found')
+//
+// }
+//
+//
+//
+//
+// })
+//
+// server.listen(3000)
