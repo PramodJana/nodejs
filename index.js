022b4ba5285ca4125f96875661c2af97ec33c2e1
PramodJana <pramodjana395@gmail.com>
2019-01-11 22:10:56 +0530

    Creating our first server

--- index.js
+++ index.js
@@ -4,10 +4,26 @@ const http = require('http')
 const server = http.createServer((request,response)=>{
 console.log(request.url)
 
-response.end('Hello NodeJs')
+if(request.url === '/about'){
+  return response.end('The About Page')
+}
+else if (request.url === '/contact'){
+  return response.end('The Contact Page')
+}
+else if(request.url==='/')
+{
+  return response.end('The Home Page')
+}
+else {
+
+  response.writeHead(404)
+  response.end('The Page was not found')
+
+}
 
 
-response.writeHead(484)
+
+
 })
 
 server.listen(3000)
