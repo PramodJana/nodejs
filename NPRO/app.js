9d05be8d441b14e4958d7bca50e073e64258c89d
Pramod Jana <32437635+PramodJana@users.noreply.github.com>
2020-03-01 21:46:18 +0530

    function expression

--- NPRO/app.js
+++ NPRO/app.js
@@ -1,5 +1,22 @@
 // console.log('Hello World!');
 
-setTimeout(function(){
-    console.log('hello World!');
-},5000);
+// setTimeout(function(){
+//     console.log('hello World!');
+// },5000);
+
+var displayMessage=function()
+{
+    console.log('This is a display message function!');
+}
+
+var calculateSquare= function(number)
+{
+    return number*number;
+}
+
+var makeDouble= function(number)
+{
+    return number*2;
+}
+
+console.log(makeDouble(calculateSquare(10)));
