505f46748ef675b12bde694282eaf2d7c6c0e580
Pramod Jana <32437635+PramodJana@users.noreply.github.com>
2020-03-02 12:13:43 +0530

    express

--- NPRO/app.js
+++ NPRO/app.js
@@ -1,53 +1,15 @@
-var http= require('http');
-var fs= require('fs');
+var express= require('express');
 
+var app= express();
 
-
-var server= http.createServer(function(req,res)
+app.get('/home',function(request,response)
 {
-    if(req.url==='/home' | req.url==='/')
-    {
-    res.writeHead(200,{'Content-Type':'text/plain'});
-    res.end('This is a home page');
-    }
-    else if(req.url==='/profile')
-    {
-        res.writeHead(200,{'Content-Type':'text/plain'});
-        res.end('This is a profile page');
-    }
-    else
-    {
-        res.writeHead(404,{'Content-Type':'text/html'});
-        var readStream= fs.createReadStream(__dirname+'/404.html');
-        readStream.pipe(res);
-    }
-    // res.writeHead(200,{'Content-Type':'text/html'});
-    // var readStream=fs.createReadStream(__dirname+'/index.html');
-    // readStream.pipe(res);
-    // res.writeHead(200,{'Content-Type':'application/json'});
-    // var studentObject={
-    //     name:'john',
-    //     city:'New York',
-    // };
-    // res.end(JSON.stringify(studentObject));
+    response.send('This is my homepage');
 });
 
-server.listen(3000,'127.0.0.1');
+app.get('/aboutus',function(request,response)
+{
+    response.send('This is a about page');
+});
 
-
-
-
-
-// var readStream= fs.createReadStream(__dirname+'/demo.txt');
-// var writeStream=fs.createWriteStream(__dirname+'/demowrite.txt');
-
-// var chunk_count=0;
-// readStream.on('data',function(d)
-// {
-//     // console.log('This is the read data'+d);
-//     writeStream.write(d);
-//     chunk_count++;
-//     console.log(chunk_count);
-// })
-
-
+app.listen(5000);
