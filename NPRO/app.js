50d24dc239d45671fb7519d83fcdba8f17ecab08
Pramod Jana <32437635+PramodJana@users.noreply.github.com>
2020-03-02 12:20:31 +0530

    Route params

--- NPRO/app.js
+++ NPRO/app.js
@@ -7,9 +7,13 @@ app.get('/home',function(request,response)
     response.send('This is my homepage');
 });
 
-app.get('/aboutus',function(request,response)
+// app.get('/aboutus',function(request,response)
+// {
+//     response.send('This is a about page');
+// });
+app.get('/posts/:id',function(request,response)
 {
-    response.send('This is a about page');
-});
+    response.send('This is post : '+request.params.id);
+})
 
 app.listen(5000);
