5fa8d402accb70a95855346e5251ea9cecacf699
Pramod Jana <32437635+PramodJana@users.noreply.github.com>
2020-03-02 11:44:46 +0530

    simple routing

--- NPRO/app.js
+++ NPRO/app.js
@@ -5,29 +5,34 @@ var fs= require('fs');
 
 var server= http.createServer(function(req,res)
 {
+    if(req.url==='/home' | req.url==='/')
+    {
+    res.writeHead(200,{'Content-Type':'text/plain'});
+    res.end('This is a home page');
+    }
+    else if(req.url==='/profile')
+    {
+        res.writeHead(200,{'Content-Type':'text/plain'});
+        res.end('This is a profile page');
+    }
+    else
+    {
+        res.writeHead(404,{'Content-Type':'text/html'});
+        var readStream= fs.createReadStream(__dirname+'/404.html');
+        readStream.pipe(res);
+    }
     // res.writeHead(200,{'Content-Type':'text/html'});
     // var readStream=fs.createReadStream(__dirname+'/index.html');
     // readStream.pipe(res);
-    res.writeHead(200,{'Content-Type':'application/json'});
-    var studentObject={
-        name:'john',
-        city:'New York',
-    };
-    res.end(JSON.stringify(studentObject));
+    // res.writeHead(200,{'Content-Type':'application/json'});
+    // var studentObject={
+    //     name:'john',
+    //     city:'New York',
+    // };
+    // res.end(JSON.stringify(studentObject));
 });
 
 server.listen(3000,'127.0.0.1');
-
-
-
-
-
-
-
-
-
-
-
 
 
 
