8bf750bf07ecce500093050cd632521097c7be26
Pramod Jana <32437635+PramodJana@users.noreply.github.com>
2020-03-02 10:49:30 +0530

    writeable stream

--- NPRO/app.js
+++ NPRO/app.js
@@ -2,29 +2,16 @@ var http= require('http');
 var fs= require('fs');
 
 var readStream= fs.createReadStream(__dirname+'/demo.txt');
+var writeStream=fs.createWriteStream(__dirname+'/demowrite.txt');
+
 var chunk_count=0;
 readStream.on('data',function(d)
 {
-    console.log('This is the read data'+d);
+    // console.log('This is the read data'+d);
+    writeStream.write(d);
     chunk_count++;
     console.log(chunk_count);
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
