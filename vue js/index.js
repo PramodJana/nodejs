8a05dfad061cc0bedfa0fa9d01d266edd132f390
Pramod Jana <32437635+PramodJana@users.noreply.github.com>
2020-04-12 19:54:09 +0530

    vue refs

--- vue js/index.js
+++ vue js/index.js
@@ -1,30 +1,33 @@
-Vue.component('mycomponent',
-    {
-        template:'<p>This is my component {{ name}} - {{temprature}} <button v-on:click="changeTemp()">Click Here</button></p> ',
-        data:function(){
-            return{
-                name:'John',
-                temprature:100,
-            }
-        },
-        methods:{
-            changeTemp:function(){
-                this.temprature+=10;
-            }
-        },
-    }
-);
+// Vue.component('mycomponent',
+//     {
+//         template:'<p>This is my component {{ name}} - {{temprature}} <button v-on:click="changeTemp()">Click Here</button></p> ',
+//         data:function(){
+//             return{
+//                 name:'John',
+//                 temprature:100,
+//             }
+//         },
+//         methods:{
+//             changeTemp:function(){
+//                 this.temprature+=10;
+//             }
+//         },
+//     }
+// );
 
 var app=new Vue(
     {
         el:'#app',
 
         data:{
-           value:100,
-           price:200,
+           years:2,
+           rate:12,
+           intr:'',
         },
         methods:{
-           
+           interest:function(){
+               this.intr=(this.$refs.principal.value)* this.years*this.rate;
+           }
             },
             
         }
